6218f095a247b9b2edc342571dab8c4d827b6bec
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-09 14:56:04 +0700

    change index

--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import getAPI from './Get';
-import PostAPI from './Post';
-import DeleteAPI from './Delete';
+// import React from 'react';
+// import getAPI from './Get';
+// import PostAPI from './Post';
+// import DeleteAPI from './Delete';
 
-const getNewsBlog = () => getAPI("posts?_sort=id&_order=asc")
-const postNewsBlog = (dataYangDikirim) => PostAPI("posts", dataYangDikirim)
-const deleteNewBlog = (dataYgDihapus) => DeleteAPI("posts", dataYgDihapus)
+// const getNewsBlog = () => getAPI("posts?_sort=id&_order=asc")
+// const postNewsBlog = (dataYangDikirim) => PostAPI("posts", dataYangDikirim)
+// const deleteNewBlog = (dataYgDihapus) => DeleteAPI("posts", dataYgDihapus)
 
-const API = {
-    getNewsBlog,
-    postNewsBlog,
-    deleteNewBlog
-}
+// const API = {
+//     getNewsBlog,
+//     postNewsBlog,
+//     deleteNewBlog
+// }
 
-export default API;
+// export default API;
