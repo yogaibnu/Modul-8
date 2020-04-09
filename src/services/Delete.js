0aafe651caa1747ea0ec1db17ec2ce39693df735
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-09 15:10:00 +0700

    change

--- src/services/Delete.js
+++ src/services/Delete.js
@@ -1,17 +1,17 @@
-import { domainPath } from './Config';
+// import { domainPath } from './Config';
 
-const DeleteAPI = (path, data) => {
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}/${data}`, {
-            method: "DELETE"
-        })
-            .then((result) => {
-                resolve(result)
-            }, err => {
-                reject(err)
-            })
-    })
-    return promise;
-}
+// const DeleteAPI = (path, data) => {
+//     const promise = new Promise((resolve, reject) => {
+//         fetch(`${domainPath}/${path}/${data}`, {
+//             method: "DELETE"
+//         })
+//             .then((result) => {
+//                 resolve(result)
+//             }, err => {
+//                 reject(err)
+//             })
+//     })
+//     return promise;
+// }
 
-export default DeleteAPI;
+// export default DeleteAPI;
